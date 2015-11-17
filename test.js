5d9dc2d6083d66593c53c3b1b6fdd047b70001ca
pemrouz <p3pedram@hotmail.com>
2015-11-17 01:40:08 +0000

    make unobtrusive

--- test.js
+++ test.js
@@ -4,12 +4,22 @@ var expect = require('chai').expect
 describe('perf', function() {
   it('should time fast fn', function() {
     var fn = String
-    expect(perf(fn)).to.be.below(1)
+    expect(perf(fn)()).to.be.below(1)
   })
 
   it('should time slow fn', function() {
     var fn = function(){ for (var i = 0; i < 1000000; i++) Math.pow(Math.random(),5) }
-    expect(perf(fn)).to.be.above(1)
+    expect(perf(fn)()).to.be.above(1)
+  })
+
+  it('should be completely unobtrustive', function() {
+    var fn = function(d){ return 'foo' + d }
+    expect(perf(fn)('5')).to.be.eql(fn('5'))
+  })
+
+  it('should be be able to add optional message', function() {
+    var fn = function(d){ return 'foo' + d }
+    expect(perf(fn, 'perf')('5')).to.be.eql(fn('5'))
   })
 
 })
