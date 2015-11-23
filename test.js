5d28041928f47e126aa91ce8dbc7ab56032b1f07
pemrouz <p3pedram@hotmail.com>
2015-11-23 00:44:26 +0000

    correct tests

--- test.js
+++ test.js
@@ -2,16 +2,6 @@ var expect = require('chai').expect
   , perf = require('./')
 
 describe('perf', function() {
-  it('should time fast fn', function() {
-    var fn = String
-    expect(perf(fn)()).to.be.below(1)
-  })
-
-  it('should time slow fn', function() {
-    var fn = function(){ for (var i = 0; i < 1000000; i++) Math.pow(Math.random(),5) }
-    expect(perf(fn)()).to.be.above(1)
-  })
-
   it('should be completely unobtrustive', function() {
     var fn = function(d){ return 'foo' + d }
     expect(perf(fn)('5')).to.be.eql(fn('5'))
