f756d24213d01fd33d6deff3fbf2b749345b70be
pemrouz <p3pedram@hotmail.com>
2017-12-08 00:37:07 +0000

    feat: resolve promises

--- test.js
+++ test.js
@@ -7,9 +7,22 @@ describe('perf', function() {
     expect(perf(fn)('5')).to.be.eql(fn('5'))
   })
 
-  it('should be be able to add optional message', function() {
+  it('should be able to add optional message', function() {
     var fn = function(d){ return 'foo' + d }
     expect(perf(fn, 'perf')('5')).to.be.eql(fn('5'))
+  })
+
+  it('should be work with promises', function(done) {
+    var fn = function(d){ return Promise.resolve().then(d => count++) }
+      , result = perf(fn, 'perf')('5')
+      , count = 0
+
+    expect(result).to.be.an.instanceOf(Promise)
+    expect(count).to.be.eql(0)
+    result.then(d => {
+      expect(count).to.be.eql(1)
+      done()
+    })
   })
 
 })
